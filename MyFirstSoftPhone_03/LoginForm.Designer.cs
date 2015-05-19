195beede2fd58d9e0587bc4e5e4768e2dec3d096
Szymon <szygmon@gmail.com>
2015-05-19 20:42:53 +0200

    2

--- MyFirstSoftPhone_03/LoginForm.Designer.cs
+++ MyFirstSoftPhone_03/LoginForm.Designer.cs
@@ -28,11 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(61, 103);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(163, 22);
+            this.tb_username.TabIndex = 0;
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.Location = new System.Drawing.Point(61, 155);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.Size = new System.Drawing.Size(164, 22);
+            this.tb_pass.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(61, 199);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(67, 29);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Zaloguj";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(152, 199);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(72, 29);
+            this.btn_register.TabIndex = 3;
+            this.btn_register.Text = "Zarejestruj";
+            this.btn_register.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nazwa użytkownika:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hasło:";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "LoginForm";
+            this.ClientSize = new System.Drawing.Size(282, 448);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_pass);
+            this.Controls.Add(this.tb_username);
+            this.Name = "LoginForm";
+            this.Text = "VoIP SM - logowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_pass;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
